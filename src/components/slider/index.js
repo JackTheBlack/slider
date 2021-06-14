c89f5e76be95d24d897f6e06423bb24c5671fa4b
JackTheBlack <ascona.nicolas@gmail.com>
2021-06-14 01:52:37 -0300

    l

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -10,13 +10,13 @@ const [i,setI]=useState(0);
 
 return(
  <div>
-          <p>{props.slides.length}</p>
+        
           <button data-testid="button-restart" disabled={i==0} onClick={()=>setI(0)}>Restart</button>
       <button data-testid="button-prev" disabled={i==0} onClick={()=>setI(i-1)}>Previous</button>
-     
+      <button   data-testid="button-next"  disabled={i===props.slides.length } onClick={()=>setI(i+1)}>Next</button>
       <h1 data-testid="title">{props.slides[i] && props.slides[i].title}</h1>
        <p  data-testid="text" >{props.slides[i] && props.slides[i].text}</p>
-       <button   data-testid="button-next"  disabled={i===props.slides.length } onClick={()=>setI(i+1)}>Next</button>
+      
   </div>
 );
 
