bc238e2e30ec4d3e7cc1b2be5add82a149235007
JackTheBlack <ascona.nicolas@gmail.com>
2021-06-14 23:55:47 -0300

    prittier changes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
-import './App.css';
-import React,{useState, Component} from "react";
-import Slider from './components/slider/index';
+import "./App.css";
+import React from "react";
+import Slider from "./components/slider/index";
 
-
-
-const title = "Calculator";
-
-
-const slides=[
+const slides = [
   {
-    "title": "title 0",
-    "text": "text 0"
-},
-{
-    "title": "title 1",
-    "text": "text 1"
-},
-{
-    "title": "title 2",
-    "text": "text 2",
-},
-{
-    "title": "title 3",
-    "text": "text 3",
-},
-{
-    "title": "title 4",
-    "text": "text 4",
-},
-]
-
+    title: "title 0",
+    text: "text 0",
+  },
+  {
+    title: "title 1",
+    text: "text 1",
+  },
+  {
+    title: "title 2",
+    text: "text 2",
+  },
+  {
+    title: "title 3",
+    text: "text 3",
+  },
+  { title: "title 4", text: "text 4" },
+];
 
 function App() {
-
-
-
-
-  return(<div>
-
-
-
-        <Slider slides={slides} />
-    </div>);
-
+  return (
+    <div>
+      <Slider slides={slides} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,27 +1,34 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from "react";
 
+function Slider(props) {
+  const [i, setI] = useState(0);
 
-
-
-function Slider(props){
-
-
-const [i,setI]=useState(0);
-
-return(
- <div>
-        
-          <button data-testid="button-restart" disabled={i==0} onClick={()=>setI(0)}>Restart</button>
-      <button data-testid="button-prev" disabled={i==0} onClick={()=>setI(i-1)}>Previous</button>
-      <button   data-testid="button-next"  disabled={i===props.slides.length } onClick={()=>setI(i+1)}>Next</button>
+  return (
+    <div>
+      <button
+        data-testid="button-restart"
+        disabled={i === 0}
+        onClick={() => setI(0)}
+      >
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        disabled={i === 0}
+        onClick={() => setI(i - 1)}
+      >
+        Previous
+      </button>
+      <button
+        data-testid="button-next"
+        disabled={i === props.slides.length}
+        onClick={() => setI(i + 1)}
+      >
+        Next
+      </button>
       <h1 data-testid="title">{props.slides[i] && props.slides[i].title}</h1>
-       <p  data-testid="text" >{props.slides[i] && props.slides[i].text}</p>
-      
-  </div>
-);
-
-
-
-
+      <p data-testid="text">{props.slides[i] && props.slides[i].text}</p>
+    </div>
+  );
 }
 export default Slider;
