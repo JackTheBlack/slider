1ee68142e8d33e039c2c46c35e5b7dd8a7cf89c1
JackTheBlack <ascona.nicolas@gmail.com>
2021-07-03 13:36:51 -0300

    antd and css

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
+import { DatePicker, message, Button } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 
 function Slider(props) {
   const [i, setI] = useState(0);
 
   return (
-    <div>
-      <button
+    <div className="component">
+      <Button
+        type="Primary"
         data-testid="button-restart"
         disabled={i === 0}
         onClick={() => setI(0)}
       >
         Restart
-      </button>
-      <button
+      </Button>
+      <Button
+        type="Primary"
         data-testid="button-prev"
         disabled={i === 0}
         onClick={() => setI(i - 1)}
       >
         Previous
-      </button>
-      <button
+      </Button>
+      <Button
+        type="Primary"
         data-testid="button-next"
         disabled={i === props.slides.length}
         onClick={() => setI(i + 1)}
       >
         Next
-      </button>
-      <h1 data-testid="title">{props.slides[i] && props.slides[i].title}</h1>
-      <p data-testid="text">{props.slides[i] && props.slides[i].text}</p>
+      </Button>
+      <div>
+        <h1 data-testid="title">{props.slides[i] && props.slides[i].title}</h1>
+        <p data-testid="text">{props.slides[i] && props.slides[i].text}</p>
+      </div>
     </div>
   );
 }
